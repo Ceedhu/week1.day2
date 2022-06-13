c046f07092b17b17b390b9e8d72d3303d15bca5a
Sridhar <107329849+Ceedhu@users.noreply.github.com>
2022-06-13 21:29:40 +0530

    Add files via upload

---
+++ TwoWheeler.java
@@ -0,0 +1,35 @@
+package week1.day2.assignments;
+
+public class TwoWheeler {
+
+	public static void main(String[] args) {
+		TwoWheeler v1= new TwoWheeler();
+		
+		
+	}
+		
+		 TwoWheeler()
+		{
+			int noofwheels=2;
+			System.out.println(noofwheels);
+			short noofmirrors=2;
+			System.out.println(noofmirrors);
+			long chasisnumber=5410675182L;
+			System.out.println(chasisnumber);
+			boolean isPunctured=false;
+			System.out.println(isPunctured);
+			String bikeName="SPLENDOR";
+			System.out.println(bikeName);
+				double runningkm=67198.5432;
+				System.out.println(runningkm);
+				
+			
+		}
+		
+		
+		
+		// TODO Auto-generated method stub
+
+	
+
+}
